fd99475d82ddb372be84d59083738aac327080f6
Thibault Gérondal <Tycale@users.noreply.github.com>
2024-11-14 13:21:15 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
+pwd
+
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
   -e ROBOT_THREADS=$ROBOT_THREADS \
@@ -10,6 +12,6 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
-  -v .:/opt/robotframework/pip-requirements.txt:Z \
+  -v $(pwd)/pip-requirements.txt:/opt/robotframework/pip-requirements.txt:Z \
   --user $(id -u):$(id -g) \
   $ROBOT_RUNNER_IMAGE
