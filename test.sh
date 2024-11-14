f208022e5f9df7763fac19cb9b4b370225326645
Thibault Gérondal <Tycale@users.noreply.github.com>
2024-11-14 12:00:27 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,17 +3,6 @@ REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
-echo "docker run --shm-size=$ALLOWED_SHARED_MEMORY \
-  -e BROWSER=$BROWSER \
-  -e ROBOT_THREADS=$ROBOT_THREADS \
-  -e PABOT_OPTIONS="$PABOT_OPTIONS" \
-  -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  -v $REPORTS_DIR:/opt/robotframework/reports:Z \
-  -v $TESTS_DIR:/opt/robotframework/tests:Z \
-  -v Resources:/opt/Resources:Z \
-  --user $(id -u):$(id -g) \
-  $ROBOT_RUNNER_IMAGE"
-
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
   -e ROBOT_THREADS=$ROBOT_THREADS \
@@ -21,6 +10,5 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
-  -v Resources:/opt/Resources:Z \
   --user $(id -u):$(id -g) \
   $ROBOT_RUNNER_IMAGE
